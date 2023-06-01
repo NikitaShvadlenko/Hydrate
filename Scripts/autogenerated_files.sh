49cbf961188ddc5bd531b67b25db847964c409a9
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-06-02 02:48:25 +1200

    Swift modules viper scripts

--- Scripts/autogenerated_files.sh
+++ Scripts/autogenerated_files.sh
@@ -3,5 +3,18 @@ eval mkdir -p $GENERATED_DIR
 eval touch $GENERATED_DIR/Strings+Generated.swift
 eval touch $GENERATED_DIR/XCAssets+Generated.swift
 
-swiftgen config run --config swiftgen.yml
+MODULES_FOLDER="./Hydrate/Packages"
 
+for MODULE in "${SWIFTGEN_MODULES[@]}"; do
+	MODULE_DIR="${MODULES_FOLDER}/${MODULE}"
+	mkdir -p "${MODULE_DIR}/Sources/${MODULE}/Generated"
+	swiftgen config run --config "${MODULE_DIR}/swiftgen.yml"
+done
+
+SOURCERY_MODULES=(
+	)
+
+for MODULE in "${SOURCERY_MODULES[@]}"; do
+	MODULE_DIR="${MODULES_FOLDER}/${MODULE}"
+	sourcery --config "${MODULE_DIR}/.sourcery.yml"
+done
