58e74585e0d49e50d3c38e6c8443a1c965694b78
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-06-04 04:01:03 +1200

    Base collectionView + flow layout

--- Hydrate/MainScreen/MainScreenView.swift
+++ Hydrate/MainScreen/MainScreenView.swift
@@ -3,6 +3,7 @@ import SnapKit
 
 final class MainScreenView: UIView {
     let progressView = HydrationProgressView()
+    let shortcutsView = ShortcutsView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,10 +21,17 @@ extension MainScreenView {
     private func configureViews() {
         backgroundColor = Asset.backgroundColor.color
         addSubview(progressView)
+        addSubview(shortcutsView)
         progressView.snp.makeConstraints { make in
             make.centerX.centerY.equalToSuperview()
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(150)
+        }
+
+        shortcutsView.snp.makeConstraints { make in
+            make.height.equalTo(100)
+            make.leading.trailing.equalToSuperview().inset(12)
+            make.bottom.equalTo(safeAreaLayoutGuide)
         }
     }
 }
---
+++ Hydrate/MainScreen/Views/ShortcutsView.swift
@@ -0,0 +1,109 @@
+import UIKit
+import SnapKit
+
+class ShortcutsView: UIView {
+
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.delegate = self
+        // TODO: create a custom cell and data source
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "\(UICollectionViewCell.self)")
+        return collectionView
+    }()
+
+    private lazy var flowLayout: UICollectionViewFlowLayout = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumInteritemSpacing = Constants.spaceBetweenCards
+        flowLayout.minimumLineSpacing = Constants.spaceBetweenCards
+        flowLayout.sectionInset = Constants.cardInsets
+        flowLayout.scrollDirection = .horizontal
+        return flowLayout
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension ShortcutsView: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        let availableWidth = bounds.width - (Constants.spaceBetweenCards * (Constants.numberOfColumns - 1))
+        - (Constants.horizontalCardInsets * 2)
+        let width = availableWidth / Constants.numberOfColumns
+
+        let availableHeight = bounds.height - (Constants.spaceBetweenCards * (Constants.numberOfRows - 1))
+        - (Constants.verticalCardInsets * 2)
+        let height = availableHeight / Constants.numberOfRows
+
+        return CGSize(width: width, height: height)
+    }
+
+    private enum Constants {
+        static let spaceBetweenCards: CGFloat = 10
+        static let horizontalCardInsets: CGFloat = 10
+        static let verticalCardInsets: CGFloat = 10
+        static let cardInsets = UIEdgeInsets(
+            top: verticalCardInsets,
+            left: horizontalCardInsets,
+            bottom: verticalCardInsets,
+            right: horizontalCardInsets
+        )
+        static let numberOfRows: CGFloat = 2
+        static let numberOfColumns: CGFloat = 2
+    }
+
+}
+
+// MARK: - Public Methods
+extension ShortcutsView {
+    var dataSource: UICollectionViewDataSource? {
+        get {
+            collectionView.dataSource
+        }
+        set {
+            collectionView.dataSource = newValue
+        }
+    }
+
+    func reloadCollection() {
+        collectionView.reloadData()
+    }
+}
+
+// MARK: Private Methods
+extension ShortcutsView {
+    private func setupView() {
+        self.backgroundColor = .clear
+        addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension ShortcutsView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(UICollectionViewCell.self)", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
