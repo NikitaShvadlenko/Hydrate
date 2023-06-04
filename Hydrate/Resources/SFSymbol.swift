939b6136f6c61c0405ee68a2666f53b739a84d19
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-06-05 03:35:10 +1200

    Added sfSymbols for tabs

---
+++ Hydrate/Resources/SFSymbol.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public enum SFSymbol: String, SFSymbolRepresentable {
+    case plusCircle = "plus.circle"
+    case settings = "gearshape"
+    case stats = "list.bullet.rectangle.portrait.fill"
+    case water = "drop.fill"
+    case achievements = "flag.2.crossed.circle"
+}
--- Hydrate/Utils/SFSymbol.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-public enum SFSymbol: String, SFSymbolRepresentable {
-    case plusCircle = "plus.circle"
-}
