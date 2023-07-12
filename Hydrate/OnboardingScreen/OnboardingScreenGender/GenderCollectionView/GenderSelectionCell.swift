87524a0923443a81ac04e19aae9ebf460641816e
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-07-12 18:14:13 +1200

    GenderSelectionView frame

--- Hydrate/OnboardingScreen/OnboardingScreenGender/GenderCollectionView/GenderSelectionCell.swift
+++ Hydrate/OnboardingScreen/OnboardingScreenGender/GenderCollectionView/GenderSelectionCell.swift
@@ -1,6 +1,21 @@
 import UIKit
 
 class GenderSelectionCell: UICollectionViewCell {
+
+    private lazy var selectionBox: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.layer.cornerRadius = 10
+        return view
+    }()
+
+    private lazy var genderImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .yellow
+        imageView.layer.cornerRadius = 10
+        return imageView
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -12,6 +27,21 @@ class GenderSelectionCell: UICollectionViewCell {
 
 extension GenderSelectionCell {
     private func setupView() {
-        contentView.backgroundColor = .green
+        [
+            selectionBox,
+            genderImageView
+        ].forEach(addSubview(_:))
+
+        selectionBox.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.height.equalTo(20)
+            make.width.equalTo(20)
+        }
+
+        genderImageView.snp.makeConstraints { make in
+            make.bottom.equalTo(selectionBox.snp.top).offset(-8)
+            make.leading.trailing.top.equalToSuperview()
+        }
     }
 }
