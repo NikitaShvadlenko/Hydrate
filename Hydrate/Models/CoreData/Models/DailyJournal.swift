53641b26d37455e5f476f9c3740de8e3fcfe7de7
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-08-11 16:05:58 +1200

    wip: DailyJournal

--- Hydrate/Models/CoreData/Models/DailyJournal.swift
+++ Hydrate/Models/CoreData/Models/DailyJournal.swift
@@ -20,26 +20,52 @@ final class DailyJournal: NSManagedObject {
         }
     }
 
-    static func insert(
-        into context: NSManagedObjectContext,
-        dailyGoal: Double
-    ) -> DailyJournal {
+    static func currentDaysJournal(context: NSManagedObjectContext) -> DailyJournal {
+        let fetchRequest = sortedFetchRequest
+        do {
+            let journalEntries = try context.fetch(fetchRequest)
+            guard let lastJournal = journalEntries.first else {
+                return createNewJournal(context: context)
+            }
+            if Calendar(identifier: .gregorian).isDateInToday(lastJournal.date) {
+                return lastJournal
+            } else {
+                return createNewJournal(context: context)
+            }
+        } catch {
+            fatalError("Failed to execute fetch request: \(error)")
+        }
+    }
+
+    private static func createNewJournal(context: NSManagedObjectContext) -> DailyJournal {
         let dailyJournal: DailyJournal = context.insertObject()
-      //  dailyJournal.dailyGoal = dailyGoal
+        dailyJournal.dailyGoal = 3000
         dailyJournal.date = Date()
+        _ = context.saveOrRollback()
         return dailyJournal
     }
 
-    static func updateDailyJournal(
-        object: DailyJournal,
-        into context: NSManagedObjectContext,
-        journalEntries: Set<JournalEntry>,
-        dailyGoal: Double
-    ) {
-        guard let updateObject = context.object(with: object.objectID) as? Self else { return }
-        // updateObject.dailyGoal = dailyGoal
-        updateObject.journalEntries = journalEntries
-    }
+//    static func insert(
+//        into context: NSManagedObjectContext,
+//        dailyGoal: Int
+//    ) -> DailyJournal {
+//        context.performC
+//        let dailyJournal: DailyJournal = context.insertObject()
+//        dailyJournal.dailyGoal = dailyGoal
+//        dailyJournal.date = Date()
+//        return dailyJournal
+//    }
+
+//    static func updateDailyJournal(
+//        object: DailyJournal,
+//        into context: NSManagedObjectContext,
+//        journalEntries: Set<JournalEntry>,
+//        dailyGoal: Int
+//    ) {
+//        guard let updateObject = context.object(with: object.objectID) as? Self else { return }
+//        updateObject.dailyGoal = dailyGoal
+//        updateObject.journalEntries = journalEntries
+//    }
 }
 
 extension DailyJournal: Managed {
