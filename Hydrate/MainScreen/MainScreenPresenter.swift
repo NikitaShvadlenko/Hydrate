36a62769842b76144c08f9405a6539af400000d5
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-08-22 13:42:30 +1200

    wip

--- Hydrate/MainScreen/MainScreenPresenter.swift
+++ Hydrate/MainScreen/MainScreenPresenter.swift
@@ -12,8 +12,6 @@ final class MainScreenPresenter {
 // MARK: - MainScreenViewOutput
 extension MainScreenPresenter: MainScreenViewOutput {
     func viewDidLoad(_ view: MainScreenViewInput) {
-   //   router?.routeToOnboardingScreen()
-//        router?.routeToSettingsScreen()
        view.configureViews()
        interactor?.retrieveUserData()
     }
