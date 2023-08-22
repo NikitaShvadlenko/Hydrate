3c8c9531bf997264c837b92323886c627630b72c
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-08-22 20:42:17 +1200

    wip

--- Hydrate/MainScreen/MainScreenView.swift
+++ Hydrate/MainScreen/MainScreenView.swift
@@ -4,6 +4,11 @@ import SnapKit
 final class MainScreenView: UIView {
     let progressView = HydrationProgressView()
     let shortcutsView = ShortcutsView()
+    private lazy var undisegnedView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .purple
+        return view
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,8 +26,16 @@ extension MainScreenView {
     private func configureViews() {
         backgroundColor = Asset.backgroundColor.color
         addSubview(progressView)
+        addSubview(undisegnedView)
+
+        undisegnedView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(24)
+            make.height.equalTo(54)
+            make.leading.trailing.equalToSuperview().inset(24)
+        }
+
         progressView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
+            make.top.equalTo(undisegnedView.snp.bottom)
             make.width.equalToSuperview().inset(20)
             make.height.equalTo(progressView.snp.width)
             make.centerX.equalToSuperview()
