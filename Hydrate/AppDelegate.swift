6077869c99853fd4c1f81798d565939fb769d354
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-08-22 13:12:53 +1200

    Removed navigaiton bar, set tab bar

--- Hydrate/AppDelegate.swift
+++ Hydrate/AppDelegate.swift
@@ -4,31 +4,75 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let container = CoreDataService.shared.container
 
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        let container = CoreDataService.shared.container
-            let window = UIWindow(frame: UIScreen.main.bounds)
-            let tabBarController = UITabBarController()
-            window.rootViewController = tabBarController
-            var viewControllers = [UIViewController]()
-            let mainScreenViewController = MainScreenAssembly.assemble(
-                managedObjectContext: container.viewContext
-            ).viewController
-            let mainScreenNavigationController = UINavigationController(
-                rootViewController: mainScreenViewController
-            )
-            // TODO: Customize tab bar
-            viewControllers.append(mainScreenNavigationController)
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let tabBarController = createTabBarController()
+        setTabBarAppearence(tabBarController: tabBarController)
+        window.rootViewController = tabBarController
+        // TODO: Customize tab bar
 
-            tabBarController.tabBar.tintColor = .black
-
-            tabBarController.setViewControllers(viewControllers, animated: false)
-
-            self.window = window
-            window.makeKeyAndVisible()
+        self.window = window
+        window.makeKeyAndVisible()
         return true
+    }
+
+    func createTabBarController() -> UITabBarController {
+        let tabBarController = UITabBarController()
+        var viewControllers = [UIViewController]()
+        let settingsViewController = SettingsScreenAssembly.assemble().viewController
+        let mainScreenViewController = MainScreenAssembly.assemble(
+            managedObjectContext: container.viewContext
+        ).viewController
+        viewControllers = [mainScreenViewController, settingsViewController]
+        tabBarController.setViewControllers(viewControllers, animated: false)
+        tabBarController.selectedViewController = mainScreenViewController
+
+        settingsViewController.tabBarItem = UITabBarItem(
+            title: L10n.TabBar.settings,
+            image: UIImage(sfSymbol: SFSymbol.settings),
+            selectedImage: UIImage(sfSymbol: SFSymbol.settings)
+        )
+
+        mainScreenViewController.tabBarItem = UITabBarItem(
+            title: L10n.TabBar.main,
+            image: UIImage(sfSymbol: SFSymbol.waterDrop),
+            selectedImage: UIImage(sfSymbol: SFSymbol.waterDrop)
+        )
+
+        return tabBarController
+    }
+
+    private func setTabBarAppearence(tabBarController: UITabBarController) {
+        let tabBarAppearance = UITabBarAppearance()
+        let tabBarScrollEdgeAppearance = UITabBarAppearance()
+        tabBarAppearance.backgroundColor = Asset.tabBarColor.color
+        tabBarScrollEdgeAppearance.backgroundColor = Asset.tabBarScrollEdgeColor.color
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabBarScrollEdgeAppearance
+        UITabBar.appearance().tintColor = .white
+        setUnselectedStackedItemsColor(
+            normal: tabBarAppearance,
+            edge: tabBarScrollEdgeAppearance,
+            color: .black
+        )
+    }
+
+    // there was no cleaner way to change unselected appearance
+    private func setUnselectedStackedItemsColor(
+        normal normalAppearance: UITabBarAppearance,
+        edge scrollEdgeAppearance: UITabBarAppearance,
+        color: UIColor
+    ) {
+        normalAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: color]
+        normalAppearance.stackedLayoutAppearance.normal.iconColor = color
+        scrollEdgeAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [
+            .foregroundColor: color
+        ]
+        scrollEdgeAppearance.stackedLayoutAppearance.normal.iconColor = color
     }
 }
--- Hydrate/Resources/SFSymbol.swift
+++ Hydrate/Resources/SFSymbol.swift
@@ -4,8 +4,8 @@ public enum SFSymbol: String, SFSymbolRepresentable {
     case plusCircle = "plus.circle"
     case settings = "gearshape"
     case stats = "list.bullet.rectangle.portrait.fill"
-    case water = "drop.fill"
     case achievements = "flag.2.crossed.circle"
     case backSymbol = "chevron.backward"
     case forwardSymbol = "chevron.forward"
+    case waterDrop = "drop"
 }
