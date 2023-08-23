5c2b25fdbea15ac4d40b8355460ed76a064e16b7
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-08-23 16:48:11 +1200

    wip main screen

--- Hydrate/MainScreen/MainScreenView.swift
+++ Hydrate/MainScreen/MainScreenView.swift
@@ -1,13 +1,27 @@
 import UIKit
 import SnapKit
 
+protocol MainScreenViewDelegate: AnyObject {
+    func viewDidPressCancelLast(_ view: MainScreenView)
+}
+
 final class MainScreenView: UIView {
     let progressView = HydrationProgressView()
     let shortcutsView = ShortcutsView()
+    weak var delegate: MainScreenViewDelegate?
+
     private lazy var undisegnedView: UIView = {
         let view = UIView()
         view.backgroundColor = .purple
         return view
+    }()
+
+    private lazy var cancelLastButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(L10n.MainScreen.cancelButtonTitle, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(viewDidPressCancelLast), for: .touchUpInside)
+        return button
     }()
 
     override init(frame: CGRect) {
@@ -27,6 +41,7 @@ extension MainScreenView {
         backgroundColor = Asset.backgroundColor.color
         addSubview(progressView)
         addSubview(undisegnedView)
+        addSubview(cancelLastButton)
 
         undisegnedView.snp.makeConstraints { make in
             make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(24)
@@ -35,10 +50,24 @@ extension MainScreenView {
         }
 
         progressView.snp.makeConstraints { make in
-            make.top.equalTo(undisegnedView.snp.bottom)
-            make.width.equalToSuperview().inset(20)
+            make.top.equalTo(undisegnedView.snp.bottom).offset(66)
+            make.width.equalTo(250)
             make.height.equalTo(progressView.snp.width)
             make.centerX.equalToSuperview()
         }
+
+        cancelLastButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(progressView.snp.bottom).offset(66)
+            make.leading.trailing.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Private Methods
+extension MainScreenView {
+    @objc
+    func viewDidPressCancelLast() {
+        delegate?.viewDidPressCancelLast(self)
     }
 }
