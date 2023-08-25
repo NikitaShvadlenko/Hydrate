b30c4cac8fa02dca40e0279580e4dcc167a890b5
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-08-25 19:47:06 +1200

    AddShorcutCell

---
+++ Hydrate/MainScreen/Views/AddShortcutCell.swift
@@ -0,0 +1,19 @@
+//
+//  AddShortcutCell.swift
+//  Hydrate
+//
+//  Created by Nikita Shvad on 25.08.2023.
+//  Copyright © 2023 Nikita Shvadlenko. All rights reserved.
+//
+
+import UIKit
+
+final class AddShortcutCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
